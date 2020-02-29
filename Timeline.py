3573ac68adc0defb7f6535dd2299cfd1c6ee95fb
movin-fingerz <shreyrg14@gmail.com>
2020-02-29 16:08:04 +0000

    add date working maybe

--- Timeline.py
+++ Timeline.py
@@ -7,21 +7,43 @@ from os import popen, path
 class EventS:
     def __init__(self, eName, day, month, year, eInfo, Tags):
         global timelineData
-        day, month, year = int(day), months.index(month), int(year)
-        self.eName, self.Date = eName, f'{day}/{month}/{year}'
+        self.eName, self.Date = eName, f'{day.zfill(2)}/{month}/{year.zfill(4)}'
         self.eInfo, self.Tags = eInfo, Tags
+
+        timelineData[self.eName] = {'date': self.Date, 'info': self.eInfo, 'tags': self.Tags}
+
         
-        self.Datet = datetime(year, month, day)
-        minn = min([timelineData[i]['date'] for i in timelineData], default = self.Datet - timedelta(days=1))
-        maxx = max([timelineData[i]['date'] for i in timelineData], default = self.Datet + timedelta(days=1))
-        sr = self.Datet-minn
-        r = maxx-minn
-        self.rat = sr/r
+def sortData():
+    global timelineData
+    kl = list(timelineData.keys())
+    vl = list(timelineData.values())
+    dl = [timelineData[i]['date'] for i in timelineData]
+    s = [datetime.strptime(i['date'], '%d/%B/%Y') for i in timelineData.values()]
+    s.sort()
 
-        timelineData[self.eName] = {'date': self.Date, 'info': self.eInfo, 'tags': self.Tags, 'rat': self.rat}
+    timelineData = {kl[dl.index(i.strftime('%d/%B/%Y'))]: vl[dl.index(i.strftime('%d/%B/%Y'))] for i in s}
 
+    kl = list(timelineData.keys())
+    vl = list(timelineData.values())
+    s = [datetime.strptime(i['date'], '%d/%B/%Y') for i in timelineData.values()]
+    unwanted = [kl[0], kl[-1]]
+    newdict = {i: timelineData[i] for i in timelineData if i not in unwanted}
+    for i in unwanted:
+        timelineData[i]['rat'] = unwanted.index(i)
+    for i in newdict:
+        minn = min(s)
+        maxx = max(s)
+        cur = datetime.strptime(newdict[i]['date'], '%d/%B/%Y')
+        num = cur - minn
+        den = maxx - minn
+        rat = num/den
+        timelineData[i]['rat'] = rat
 
-
+def save():
+    sortData()
+    with open('timelineData.json', 'w') as f:
+        d = dumps(timelineData)
+        f.write(d)
 
 def do_popup(event, popup):
     try:
@@ -29,23 +51,22 @@ def do_popup(event, popup):
     finally:
         popup.grab_release()
 
-def addDate(day, month, year, name, e4, root):
+def addDate(day, month, year, name, e4, root, timeline, width, height):
     dval = day.get()
     mval = month.get()
     yval = year.get()
     nval = name.get()
     ival = e4.get('1.0', END)
-    print(dval, mval, yval, nval, ival)
     if(dval == 'Unknown'):
         addRange()
     elif(mval == 'Unknown'):
         addRange()
     else:
         new = EventS(nval, dval, mval, yval, ival, [])
+    save()
+    refresh(timeline, width, height)
 
-    root.update()
-
-def subAdd(root):
+def subAdd(root, timeline, width, height):
     dateR = Tk()
     l0 = Label(dateR, text='Enter Name:   ')
     l1 = Label(dateR, text='Enter Day:   ')
@@ -68,11 +89,32 @@ def subAdd(root):
     e2.grid(row=2, column=1)
     e3.grid(row=3, column=1)
     e4.grid(row=4, column=1, padx=25, pady=10)
-    b1 = ttk.Button(dateR, text='Add', width=42, command=lambda: addDate(e1, e2, e3, e0, e4, root))
+    b1 = ttk.Button(dateR, text='Add', width=42, command=lambda: addDate(
+        e1, e2, e3, e0, e4, root, timeline, width, height))
     b1.grid(row=5, columnspan=2, pady=10)
 
     dateR.mainloop()
 
+def refresh(timeline, width, height):
+    for i in timelineData:
+        r = timelineData[i]['rat']
+        xc = 10 + (r*(width-20))
+        y1 = (height//2)-4
+        y2 = (height//2)+4
+        point = timeline.create_oval(xc-4, y1, xc+4, y2, fill='black')
+        txt = timeline.create_text(xc, y1-25, anchor='w', text=i, angle=90)
+
+def check_data():
+    global timelineData
+    folder = popen('chdir').read().strip('\n')
+    if(not path.exists(folder+ r'\timelineData.json')):
+        with open('timelineData.json', 'w') as f:
+            d = dumps({})
+            f.write(d)
+    else:
+        with open('timelineData.json', 'r') as f:
+            d = f.read()
+            timelineData = loads(d)
 
 def main():
     root = Tk()
@@ -83,7 +125,7 @@ def main():
     height = root.winfo_height()
 
     popup = Menu(root, tearoff=0)
-    popup.add_command(label="Add Date", command= lambda: subAdd(root))
+    popup.add_command(label="Add Date", command= lambda: subAdd(root, timeline, width, height))
     popup.add_command(label="Add Range")
     popup.add_command(label="Help")
 
@@ -94,32 +136,10 @@ def main():
     line = timeline.create_line(10, height//2, width-10, height//2, fill='black', width='3')
     buffer2 = timeline.create_line(width-10, height//2 + 20, width-10, height//2 - 20, fill='black', width='3')
 
-    for i in timelineData:
-        r = timelineData[i]['rat']
-        xc = 10 + (r*(width-20))
-        y1 = (height//2)-4
-        y2 = (height//2)+4
-        point = timeline.create_oval(xc-4, y1, xc+4, y2, fill='black')
-        txt = timeline.create_text(xc, y1-5, anchor='w', text=i, angle=90)
+    refresh(timeline, width, height)
 
     root.bind("<Button-3>", lambda x: do_popup(x, popup))
     root.mainloop()
-
-
-def check_data():
-    global timelineData
-    folder = popen('chdir').read().strip('\n')
-    if(not path.exists(folder+ r'\timelineData.json')):
-        with open('timelineData.json', 'w') as f:
-            pass
-    else:
-        with open('timelineData.json', 'r') as f:
-            d = f.read()
-            timelineData = loads(d)
-def save():
-    with open('timelineData.json', 'w') as f:
-        d = dumps(timelineData)
-        f.write(d)
 
 
 timelineData = {}
@@ -127,5 +147,4 @@ months = ['January', 'February', 'March', 'April', 'May', 'June',
  'July', 'August', 'September', 'October', 'November', 'December']
 check_data()
 main()
-save()
 
